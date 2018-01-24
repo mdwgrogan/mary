756cc5d39f49bf7005ab3ef6007106c665aafb9b
Michael Grogan <mdwgrogan@gmail.com>
2018-01-23 23:14:24 -0500

    Do-nothing installer that adds pandas dependency

---
+++ mary.py
@@ -0,0 +1,8 @@
+"""Lambda with pandas.
+"""
+import pandas
+
+
+def pandler(event, context):
+    """Convert an excel file to a csv file in the same bucket."""
+    pass
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='mary',
+        version='0.0.1',
+        install_requires=['pandas'],
+        py_modules=['mary']
+    )
